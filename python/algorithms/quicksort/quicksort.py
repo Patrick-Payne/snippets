ddb3978dab2078d480e4ee2153b78fc72b2805b2
Patrick Payne <pat.j.payne@gmail.com>
2013-09-17 00:22:16 -0400

    Added an in-place implementation of quicksort.

--- python/algorithms/quicksort/quicksort.py
+++ python/algorithms/quicksort/quicksort.py
@@ -10,6 +10,8 @@ Description: A basic recursive quicksort algorithm for python lists.
 def quick_sort(in_list):
     """
     Sorts a list recursively in increasing order using the quicksort algorithm.
+    This version of quicksort is NOT in place, and is space inefficient. See
+    in_place_quick_sort().
     """
     # Check for the base case.
     if len(in_list) <= 1:
@@ -31,3 +33,57 @@ def quick_sort(in_list):
 
     # Concatenate the sorted sublists.
     return quick_sort(smaller) + [pivot] + quick_sort(bigger)
+
+
+def in_place_quick_sort(in_list, left=0, right=None):
+    """
+    Sorts the subarray located at in_list[left:right + 1] in increasing order,
+    in place. If no left and right arguments are provided, sort the whole list.
+    """
+    # Check whether right argument was omitted.
+    if right is None:
+        right = len(in_list) - 1
+
+    # Check for the base case.
+    if right - left <= 0:
+        return
+
+    # Select a pivot. We choose one roughly in the middle of the subarray.
+    pivot_index = left + (right - left) // 2
+
+    # Partition the array.
+    partition_index = partition(in_list, left, right, pivot_index)
+
+    # Sort the smaller and bigger subarrays.
+    in_place_quick_sort(in_list, left, partition_index - 1)
+    in_place_quick_sort(in_list, partition_index + 1, right)
+
+
+def partition(in_list, left, right, pivot_index):
+    """
+    Partitions the elements in the subarray in_list[left:right+1] such that
+    elements less than the pivot value at in_list[pivot_index] are to the left
+    if it, and all greater or equal elements are placed to it's right. The
+    final index of the pivot is returned.
+    """
+    # Copy the pivot to the end of the list.
+    pivot = in_list[pivot_index]
+    swap(in_list, pivot_index, right)
+
+    # Iterate over all oter elements, putting smaller elements to the left.
+    partition_index = left
+    for index in range(left, right):
+        if in_list[index] < pivot:
+            swap(in_list, partition_index, index)
+            partition_index += 1
+
+    # Put the pivot value back into its place.
+    swap(in_list, partition_index, right)
+    return partition_index
+
+
+def swap(in_list, index1, index2):
+    """ Swaps the values stored at index1 and index2 of the list in_list."""
+    temp = in_list[index1]
+    in_list[index1] = in_list[index2]
+    in_list[index2] = temp
--- python/algorithms/quicksort/test_quicksort.py
+++ python/algorithms/quicksort/test_quicksort.py
@@ -6,7 +6,7 @@ Date Created: Sep 15, 2013
 Description: Unit tests for the quicksort algorithm.
 '''
 import unittest
-from quicksort import quick_sort
+from quicksort import quick_sort, in_place_quick_sort
 
 
 class TestQuickSort(unittest.TestCase):
@@ -43,6 +43,55 @@ class TestQuickSort(unittest.TestCase):
         # Odd length list
         self.assertEqual(quick_sort([0, 10, 11, 15, 25]), [0, 10, 11, 15, 25])
         self.assertEqual(quick_sort([15, 10, 25, 0, 11]), [0, 10, 11, 15, 25])
+
+
+class TestInPlaceQuickSort(unittest.TestCase):
+    """Tests the in-place version of the quicksort algorithm."""
+    def test_two_elements(self):
+        """Tests cases where the list has only 2 elements."""
+        test_list = [0, 0]
+        in_place_quick_sort(test_list)
+        self.assertEqual(test_list, [0, 0])
+
+        test_list = [0, 1]
+        in_place_quick_sort(test_list)
+        self.assertEqual(test_list, [0, 1])
+
+        test_list = [1, 0]
+        in_place_quick_sort(test_list)
+        self.assertEqual(test_list, [0, 1])
+
+    def test_multiple_elements(self):
+        """Tests cases where we have more than 2 elements."""
+        # Even length lists
+        test_list = [-100, 100, -5, 4]
+        in_place_quick_sort(test_list)
+        self.assertEqual(test_list, [-100, -5, 4, 100])
+
+        test_list = [1, 2, 3, 4, 5, 6]
+        in_place_quick_sort(test_list)
+        self.assertEqual(test_list, [1, 2, 3, 4, 5, 6])
+
+        test_list = [6, 5, 4, 3, 2, 1]
+        in_place_quick_sort(test_list)
+        self.assertEqual(test_list, [1, 2, 3, 4, 5, 6])
+
+        test_list = [1, 5, 2, 6, 3, 7, 4, 8, 5, 9]
+        in_place_quick_sort(test_list)
+        self.assertEqual(test_list, [1, 2, 3, 4, 5, 5, 6, 7, 8, 9])
+
+        test_list = [9, 8, 7, 6, 5, 4, 3, 2, 1]
+        in_place_quick_sort(test_list)
+        self.assertEqual(test_list, [1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+        # Odd length list
+        test_list = [9, 8, 7, 6, 5, 4, 3, 2]
+        in_place_quick_sort(test_list)
+        self.assertEqual(test_list, [2, 3, 4, 5, 6, 7, 8, 9])
+
+        test_list = [15, 10, 25, 0, 11]
+        in_place_quick_sort(test_list)
+        self.assertEqual(test_list, [0, 10, 11, 15, 25])
 
 
 if __name__ == '__main__':

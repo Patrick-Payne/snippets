e40ba7f9846700a2a551d089ccd195ac5347fae8
Patrick Payne <pat.j.payne@gmail.com>
2013-09-20 21:52:12 -0400

    Add definitions and stubs for C heap functions.
    - Added the definitions and empty stub implementations of the
      public interface functions for the C implementation of array-based
      max-heaps.

--- c/data_structures/in_place_heap/in_place_heap.c
+++ c/data_structures/in_place_heap/in_place_heap.c
@@ -8,3 +8,107 @@
  *****************************************************************************/ 
 
 #include "in_place_heap.h"
+
+/******************************************************************************
+ * Structs
+ *****************************************************************************/ 
+
+struct ArrayHeap {
+  int (*array)[];
+  int size;
+  int capacity;
+};
+
+
+/******************************************************************************
+ * Public Function Definitions
+ *****************************************************************************/ 
+
+/*! @brief Creates an empty ArrayHeap using the provided array.
+ *  @param array The array that will be used to contain the heap.
+ *  @param capacity The total capacity of the array.
+ *  @return The empty ArrayHeap.
+ *
+ *  @note: The responsibility for allocating AND deallocating the memory for
+ *    the array rests entirely on the caller.
+ */
+ArrayHeap CreateArrayHeap(int *array, int capacity) {
+  ArrayHeap new_heap;
+  return new_heap;
+}
+
+
+/*! @brief Creates a heap out of a given array of integer elements.
+ *  @param array The array that will be used to contain the heap.
+ *  @param capacity The total array capacity to be used for the heap.
+ *  @param size The number of elements already in the array to consider as part
+ *         of the heap.
+ *  @return The new ArrayHeap.
+ *
+ *  @note: The responsibility for allocating AND deallocating the memory for
+ *    the array rests entirely on the caller.
+ */
+ArrayHeap Heapify(int *array, int capacity, int size) {
+  ArrayHeap new_heap;
+  return new_heap;
+}
+
+
+/*! @brief Finds the maximum element of the ArrayHeap, without removing it.
+ *  @param heap A pointer to the ArrayHeap in question.
+ *  @return The integer value of the maximum element in the ArrayHeap
+ *
+ *  @pre There must be at least 1 element in the heap.
+ */
+int FindMax(ArrayHeap *heap) {
+  return 0;
+}
+
+
+/*! @brief Finds the minimum element of the ArrayHeap, without removing it.
+ *  @param heap A pointer to the ArrayHeap in question.
+ *  @return The integer value of the minimum element in the ArrayHeap
+ *
+ *  @pre There must be at least 1 element in the heap.
+ */
+int FindMin(ArrayHeap *heap) {
+  return 0;
+}
+
+
+/*! @brief Deletes and returns the maximum element of the ArrayHeap.
+ *  @param heap A pointer to the ArrayHeap in question.
+ *  @return The integer value of the maximum element in the heap.
+ *
+ *  @pre There must be at least 1 element in the heap.
+ */
+int PopMax(ArrayHeap *heap) {
+  return 0;
+}
+
+
+/*! @brief Deletes and returns the minimum element of the ArrayHeap.
+ *  @param heap A pointer to the ArrayHeap in question.
+ *  @return The integer value of the minimum element in the heap.
+ *
+ *  @pre There must be at least 1 element in the heap.
+ */
+int PopMin(ArrayHeap *heap) {
+  return 0;
+}
+
+
+/*! @brief Inserts an integer into an existing ArrayHeap.
+ *  @param heap A pointer to the ArrayHeap in question.
+ *  @param new_item The new integer to add to the heap.
+ *
+ *  @pre The ArrayHeap must have the capacity for the additional item.
+ */
+void InsertItem(ArrayHeap *heap, int new_item) {
+  return;
+}
+
+
+/******************************************************************************
+ * Private Function Definitions
+ *****************************************************************************/ 
--- c/data_structures/in_place_heap/in_place_heap.h
+++ c/data_structures/in_place_heap/in_place_heap.h
@@ -2,12 +2,91 @@
  * File: in_place_heap.h
  * Author: Patrick Payne
  * Date Created: Sep 20, 2013
- * Purpose: Implements a heap using an array, allow one to turn an array into
- *    a heap in place, and vice versa.
+ * Purpose: Implements a max-heap using an array, allow one to turn an array
+ *   into a heap in place, and vice versa.
  * Copyright 2013 by Patrick Payne.
  *****************************************************************************/ 
 
 #ifndef SNIPPET_C_IN_PLACE_HEAP_H_
 #define SNIPPET_C_IN_PLACE_HEAP_H_
+
+/******************************************************************************
+ * Type Declarations
+ *****************************************************************************/ 
+
+typedef struct ArrayHeap ArrayHeap;
+
+/******************************************************************************
+ * Public Function Declarations
+ *****************************************************************************/ 
+
+/*! @brief Creates an empty ArrayHeap using the provided array.
+ *  @param array The array that will be used to contain the heap.
+ *  @param capacity The total capacity of the array.
+ *  @return The empty ArrayHeap.
+ *
+ *  @note: The responsibility for allocating AND deallocating the memory for
+ *    the array rests entirely on the caller.
+ */
+ArrayHeap CreateArrayHeap(int *array, int capacity);
+
+
+/*! @brief Creates a heap out of a given array of integer elements.
+ *  @param array The array that will be used to contain the heap.
+ *  @param capacity The total array capacity to be used for the heap.
+ *  @param size The number of elements already in the array to consider as part
+ *         of the heap.
+ *  @return The new ArrayHeap.
+ *
+ *  @note: The responsibility for allocating AND deallocating the memory for
+ *    the array rests entirely on the caller.
+ */
+ArrayHeap Heapify(int *array, int capacity, int size);
+
+
+/*! @brief Finds the maximum element of the ArrayHeap, without removing it.
+ *  @param heap A pointer to the ArrayHeap in question.
+ *  @return The integer value of the maximum element in the ArrayHeap
+ *
+ *  @pre There must be at least 1 element in the heap.
+ */
+int FindMax(ArrayHeap *heap);
+
+
+/*! @brief Finds the minimum element of the ArrayHeap, without removing it.
+ *  @param heap A pointer to the ArrayHeap in question.
+ *  @return The integer value of the minimum element in the ArrayHeap
+ *
+ *  @pre There must be at least 1 element in the heap.
+ */
+int FindMin(ArrayHeap *heap);
+
+
+/*! @brief Deletes and returns the maximum element of the ArrayHeap.
+ *  @param heap A pointer to the ArrayHeap in question.
+ *  @return The integer value of the maximum element in the heap.
+ *
+ *  @pre There must be at least 1 element in the heap.
+ */
+int PopMax(ArrayHeap *heap);
+
+
+/*! @brief Deletes and returns the minimum element of the ArrayHeap.
+ *  @param heap A pointer to the ArrayHeap in question.
+ *  @return The integer value of the minimum element in the heap.
+ *
+ *  @pre There must be at least 1 element in the heap.
+ */
+int PopMin(ArrayHeap *heap);
+
+
+/*! @brief Inserts an integer into an existing ArrayHeap.
+ *  @param heap A pointer to the ArrayHeap in question.
+ *  @param new_item The new integer to add to the heap.
+ *
+ *  @pre The ArrayHeap must have the capacity for the additional item.
+ */
+void InsertItem(ArrayHeap *heap, int new_item);
+
 
 #endif /* SNIPPET_C_IN_PLACE_HEAP_H_ */

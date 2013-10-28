4b6ca49eea5d840b0f201bd8c0553dc57599e701
Patrick Payne <pat.j.payne@gmail.com>
2013-10-27 20:37:24 -0400

    Fix formatting in quick select algorithm.

--- c/algorithms/quick_select/quick_select.c
+++ c/algorithms/quick_select/quick_select.c
@@ -21,34 +21,33 @@
  *  @return The kth smallest integer in list.
  */
 int QuickSelect(int *list, int size, int k) {
-    // Check that the inputs are valid
-    assert(list != NULL);
-    assert(size > 0);
-    assert(k > 0);
-    assert(size >= k);
+  // Check that the inputs are valid
+  assert(list != NULL);
+  assert(size > 0);
+  assert(k > 0);
+  assert(size >= k);
 
-     int left = 0;
-    int right = size - 1;
+  int left = 0;
+  int right = size - 1;
 
-     while (true) {
-        // Choose a pivot, and partition the array.
-        int pivot = left;
-        pivot = Partition(list, left, right, pivot);
-
-        // Determine how many elements are smaller or equal to
-        // the pivot element.
-        int pivot_position = pivot + 1;
-        if (pivot_position > k) {
-            // There are more than k-1 smaller elements.
-            right = pivot - 1;
-        } else if (pivot_position < k) {
-            // There are fewer than k-1 smaller elements.
-            left = pivot + 1;
-         } else {
-            // We have found the kth item.
-            return list[pivot];
-        }
+  while (true) {
+    // Choose a pivot, and partition the array.
+    int pivot = left;
+    pivot = Partition(list, left, right, pivot);
+    // Determine how many elements are smaller or equal to
+    // the pivot element.
+    int pivot_position = pivot + 1;
+    if (pivot_position > k) {
+      // There are more than k-1 smaller elements.
+        right = pivot - 1;
+    } else if (pivot_position < k) {
+      // There are fewer than k-1 smaller elements.
+      left = pivot + 1;
+    } else {
+      // We have found the kth item.
+      return list[pivot];
     }
+  }
 }
 
 
